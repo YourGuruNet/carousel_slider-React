2620548067ef234add44d484c00f954757a879c9
yourgurunet <info@yourgurunet.com>
2021-01-22 11:03:49 +0200

    Base setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import image1 from './images/image1.jpg';
+import image2 from './images/image1.jpg';
+import image3 from './images/image1.jpg';
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Section>
+          <div className='mySlides fade'>
+            <img src={image1} style={{ width: '100%' }} />
+          </div>
+        </Section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
+
+const Section = styled.div`
+  max-width: 100rem;
+  position: relative;
+  margin: auto;
+`;
